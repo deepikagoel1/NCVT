e65b4fdd5ad15062e9e0a3bc371720fcc1853c8b
DEEPIKA GOEL <goeld9911@gmail.com>
2022-08-07 22:29:44 +0530

    Final Dashboard in app4.py

--- app4.py
+++ app4.py
@@ -1,14 +1,33 @@
 import streamlit as st
 import pandas as pd
 import time
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
+from streamlit_card import card
+
+
+@st.cache
+def data_upload():
+    df = pd.read_csv(r'C:\Users\goeld\Downloads\NCVT_MIS_DASH\iti_trades.csv')
+    return df
+
+st.set_page_config(
+    page_title="Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
 
 st.title("NCVT MIS Dashboard")
 
+df = data_upload()
 
-df = pd.read_csv(r'C:\Users\goeld\Downloads\NCVT_MIS_DASH\iti_trades.csv')
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_default_column(editable = True, groupable = True)
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
 
-left_column, middle_column, right_column = st.columns(3, gap = 'medium')
+cols = st.columns([.333, .333, .333])
 
 
 placeholder = st.empty()    
@@ -17,75 +36,334 @@ placeholder = st.empty()
 states = list(df['State_Name'].unique())
 state = st.sidebar.multiselect("State",options = states)
 
-
-if state:
-    df = df[df['State_Name'].isin(state)] 
-    placeholder.dataframe(df.reset_index())
-
+with placeholder:
+    if state:
+        df = df[df['State_Name'].isin(state)] 
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    ) 
 
 districts = list(df['District_Name'].unique())
 district = st.sidebar.multiselect("District",options = districts)
 
-if district:
-    df = df[df['District_Name'].isin(district)]
-    placeholder.dataframe(df.reset_index())
+with placeholder:
+    if district:
+        df = df[df['District_Name'].isin(district)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    )    
+        # placeholder.dataframe(grid_table)
     
-durations = list(df['Course_Duration'].drop_duplicates())
+durations = list(df['Course_Duration'].unique())
 duration = st.sidebar.multiselect("Course Duration",options = durations)
 
-if duration:
+
+with placeholder:
+    if duration:
+        
+        df = df[df['Course_Duration'].isin(duration)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    ) 
+        # placeholder.dataframe(grid_table)
     
-    df = df[df['Course_Duration'].isin(duration)]
-    placeholder.dataframe(df.reset_index())
-    
-years = list(df['Year'].drop_duplicates())
+years = list(df['Year'].unique())
 year = st.sidebar.multiselect('Year',options = years)
 
-if year:
-    df = df[df['Year'].isin(year)]
-    placeholder.dataframe(df.reset_index())
+
+with placeholder:
+    if year:
+        df = df[df['Year'].isin(year)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    )   
+        # placeholder.dataframe(grid_table)
     
-iti_names = list(df['ITI_Name'].drop_duplicates())
+iti_names = list(df['ITI_Name'].unique())
 iti_name = st.sidebar.multiselect('ITI Name',options = iti_names)
 
-if iti_name:
-    df = df[df['ITI_Name'].isin(iti_name)]
-    placeholder.dataframe(df.reset_index())
+with placeholder:
+    if iti_name:
+        df = df[df['ITI_Name'].isin(iti_name)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    )   
+        # placeholder.dataframe(grid_table)
     
-iti_categories = list(df['ITI_Category'].drop_duplicates())
+iti_categories = list(df['ITI_Category'].unique())
 iti_category = st.sidebar.multiselect('ITI Category',options = iti_categories)
 
-if iti_category:
-    df = df[df['ITI_Category'].isin(iti_category)]
-    placeholder.dataframe(df.reset_index())
+with placeholder:
+    if iti_category:
+        df = df[df['ITI_Category'].isin(iti_category)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    ) 
+        # placeholder.dataframe(grid_table)
     
-iti_nstis = list(df['Details'].drop_duplicates())
+iti_nstis = list(df['Details'].unique())
 iti_nsti = st.sidebar.multiselect('ITI/NSTI',options = iti_nstis)
 
-if iti_nsti:
-    df = df[df['Details'].isin(iti_nsti)]
-    placeholder.dataframe(df.reset_index())
+with placeholder:
+    if iti_nsti:
+        df = df[df['Details'].isin(iti_nsti)]
+        # placeholder.dataframe(df.reset_index())
+        AgGrid(df,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='VALUE_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+    )  
+        # placeholder.dataframe(grid_table)
     
 total_seats = int(df["Totalseats"].sum())
 seats_available = int(df["SeatsAvailable"].sum())
 total_units = int(df["TotalUnits"].sum())
 units_available = int(df["UnitsAvailable"].sum())
 admissions = int(df["Admissions"].sum())
+num_itis = int(df['ITI_Name'].nunique())
 
 
-with left_column:
-    st.subheader("Total Seats")
-    st.subheader(f"{total_seats}")
-    st.subheader("Seats Available")
-    st.subheader(f"{seats_available}")  
-with middle_column:
-    st.subheader("Total Units")
-    st.subheader(f"{total_units}")
-    st.subheader("Units Available")
-    st.subheader(f"{units_available}")
-with right_column:
-    st.subheader("Admissions")
-    st.subheader(f"{admissions}")
+
+with cols[0]:
+    # st.subheader("Total Seats")
+    # st.subheader(f"{total_seats}")
+    # st.subheader("Seats Available")
+    # st.subheader(f"{seats_available}")  
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Total Seats"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{total_seats}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Seats Available"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{seats_available}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+    # card("Total Seats", f"{total_seats}")
+    # card("Seats Available", f"{seats_available}")
+    
+with cols[1]:
+    # st.subheader("Total Units")
+    # st.subheader(f"{total_units}")
+    # st.subheader("Units Available")
+    # st.subheader(f"{units_available}")
+    # card("Total Units", f"{total_units}")
+    # card("Units Available", f"{units_available}")
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Total Units"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{total_units}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+    
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Units Available"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{units_available}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+    
+    
+with cols[2]:
+    # st.subheader("Admissions")
+    # st.subheader(f"{admissions}")
+    # card("Admissions", f"{admissions}")
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Admissions"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{admissions}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+    
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Number of ITI"
+    # lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = f"{num_itis}"
+
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
+
 
 def convert_df(df):
        return df.to_csv().encode('utf-8')
@@ -100,3 +378,5 @@ st.download_button(
    "text/csv",
    key='download-csv'
 )
+
+
--- app6.py
+++ app6.py
@@ -1,8 +1,9 @@
 from dash import Dash, dash_table,html
 import pandas as pd
 from collections import OrderedDict
-
-
+from dash.dependencies import Input, Output
+import dash.dash_table as dt
+import dash_bootstrap_components as dbc
 
 df = pd.read_csv('iti_trades.csv')
 
@@ -10,21 +11,7 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     dash_table.DataTable(
-        id='table-dropdown',
-        dropdown= {
-            'State': {
-                'options': [
-                    {'label': i, 'value': i}
-                    for i in df['State_Name'].unique()
-                ]
-            },
-            'Trades': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in df['Trade_Name'].unique()
-                ]
-            }
-        },
+        
         data=df.to_dict('records'),
         columns=[
             {'id': c, 'name': c}
@@ -66,6 +53,27 @@ app.layout = html.Div([
     ),
     html.Div(id='table-dropdown-container')
 ])
+
+@app.callback(
+    Output('table-dropdown-container', 'data'),
+    [Input('Filter1_dropwdown', 'value'),
+     Input('Filter2_dropwdown', 'value'),
+     Input('Filter3_dropwdown', 'value'),
+     Input('Filter4_dropwdown', 'value'),
+     Input('Filter5_dropwdown', 'value'),
+     Input('Filter6_dropwdown', 'value'),
+     Input('Filter7_dropwdown', 'value')
+    ]
+    )
+
+def filter_df(filter_01, filter_02, filter_03, filter_04, filter_05, filter_06, filter_07):
+    if (not filter_01 and not filter_02 and not filter_03 and not filter_04 and not filter_05 and not filter_06 and not filter_07):
+        # Return all the rows on initial load
+        return df.to_dict('records')
+
+
+    filtered = df.query('filter_1_field in @filter_01 or filter_2_field in @filter_02 or filter_3_field in @filter_03 or filter_4_field in @filter_04 or filter_5_field in @filter_05 or filter_6_field in @filter_06 or filter_7_field in @filter_07')
+    return filtered.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
